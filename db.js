f6662f6ea55778b5db9a9725e09353705169271a
Aishwarya <wagdarkar.a@northeaster.edu>
2022-12-01 11:29:41 -0500

    connections

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 var mongoURL = 'mongodb+srv://Srujan:Foodxproject@cluster0.p56g0vl.mongodb.net/FoodX'
+// var mongoURL = 'mongodb+srv://Srujan:Foodxproject@cluster0.p56g0vl.mongodb.net/FoodX?authSource=Srujan'
 
 mongoose.connect(mongoURL, {useUnifiedTopology:true, useNewUrlParser:true})
 
