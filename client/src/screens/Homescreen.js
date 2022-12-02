2eab715e334f0832a49a802ca287a56d092e7dad
saisrujanpenta <penta.s@northeastern.edu>
2022-12-01 23:31:56 -0500

    project

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,21 +1,39 @@
-import React from 'react'
-import Pizza from '../components/Pizza'
-import pizzas from '../pizzasdata'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPizzas } from "../actions/pizzaActions";
+import Pizza from "../components/Pizza";
+
 
 export default function Homescreen() {
+  const dispatch = useDispatch();
+
+  const pizzasstate = useSelector((state) => state.getAllPizzasReducer);
+
+  const { pizzas, error, loading } = pizzasstate;
+
+  useEffect(() => {
+    dispatch(getAllPizzas());
+  }, []);
+
   return (
     <div>
-        .
-        <div className="row">
-            {pizzas.map(pizza=>{
-                return <div className='col-md-4'>
-                    <div>
-                        <Pizza pizza={pizza}/>
-                    </div>
+      <div className="row">
+        {loading ? (
+          <h1>Loading</h1>
+        ) : error ? (
+          <h1>Something went wrong</h1>
+        ) : (
+          pizzas.map((pizza) => {
+            return (
+              <div className="col-md-4">
+                <div>
+                  <Pizza pizza={pizza} />
                 </div>
-            })}
-        </div>
-        
+              </div>
+            );
+          })
+        )}
+      </div>
     </div>
-  )
+  );
 }
---
+++ routes/pizzasRoute.js
@@ -0,0 +1,18 @@
+const { response } = require("express");
+const express = require("express");
+const router = express.Router();
+const Pizza = require('../models/pizzaModel')
+
+router.get("/getallpizzas", async(req, res) => {
+
+    try{
+        const pizzas = await Pizza.find({})
+        response.send(pizzas)
+    }catch(error){
+        return res.status(400).json({message: error});
+
+    }
+
+});
+
+module.exports = router;
