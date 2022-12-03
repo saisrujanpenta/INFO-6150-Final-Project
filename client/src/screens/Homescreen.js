85e85260eae820f270dbd7aa1a1d263f2905ebbc
Aishwarya <wagdarkar.a@northeaster.edu>
2022-12-02 21:12:10 -0500

    route changes

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import Pizza from '../components/Pizza'
 import pizzas from '../pizzasdata'
-
 export default function Homescreen() {
+
+    const dispatch =useDispatch()
+    useEffect(() => {
+        dispatch(getAllPizzas())
+
+    }, [])
+
   return (
     <div>
-        .
+        
         <div className="row">
             {pizzas.map(pizza=>{
                 return <div className='col-md-4'>
--- server.js
+++ server.js
@@ -7,23 +7,25 @@ const db = require("./db.js");
 const pizzaModel = require("./models/pizzaModel.js");
 app.use(express.json());
 
+const pizzasRoute =require('./routes/pizzasRoute')
 
+app.use('/api/pizzas/', pizzasRoute)
 
 app.get("/", (req, res) => {
     res.send("Server working!" + port);
 });
 
 
-app.get("/getpizzas", (req, res)=>{
-    Pizza.find({} , (err , docs)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.send(docs)
-        }
-    })
-});
+// app.get("/getpizzas", (req, res)=>{
+//     Pizza.find({} , (err , docs)=>{
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             res.send(docs)
+//         }
+//     })
+// });
 
 const port = process.env.PORT || 8000;
 
